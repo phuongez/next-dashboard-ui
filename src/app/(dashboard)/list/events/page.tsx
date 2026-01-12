43d552552c8403d53311a175c35d69659a9c6d62
phuongez <duyphuong.hvnh@gmail.com>
2026-01-12 15:59:27 +0700

    move role inside function

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import FormContainer from "@/components/FormContainer";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
@@ -15,86 +13,87 @@ type EventList = Event & {
   class: Class;
 };
 
-const { userId, sessionClaims } = await auth();
-const role = (sessionClaims?.metadata as { role?: string })?.role;
-const currentUserId = userId;
-
-const columns = [
-  {
-    header: "Tên sự kiện",
-    accessor: "title",
-  },
-  {
-    header: "Lớp",
-    accessor: "class",
-    // className: "hidden md:table-cell",
-  },
-  {
-    header: "Ngày",
-    accessor: "date",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Bắt đầu",
-    accessor: "startTime",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Kết thúc",
-    accessor: "endTime",
-    className: "hidden md:table-cell",
-  },
-  ...(role === "admin"
-    ? [
-        {
-          header: "Actions",
-          accessor: "action",
-        },
-      ]
-    : []),
-];
-
-const renderRow = (item: EventList) => (
-  <tr
-    key={item.id}
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaSkyLight"
-  >
-    <td className="flex items-center gap-4 p-4">{item.title}</td>
-    <td className="">{item.class?.name || "-"}</td>
-    <td className="hidden md:table-cell">
-      {new Intl.DateTimeFormat("vi-VN").format(item.startTime)}
-    </td>
-    <td className="hidden md:table-cell">
-      {item.startTime.toLocaleString("vi-VN", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      })}
-    </td>
-    <td className="hidden md:table-cell">
-      {item.endTime.toLocaleString("vi-VN", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      })}
-    </td>
-    <td>
-      <div className="flex items-center gap-2">
-        {role === "admin" && (
-          <>
-            <FormContainer table="event" type="update" data={item} />
-            <FormContainer table="event" type="delete" id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
 const EventListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string } | undefined;
 }) => {
+  const { userId, sessionClaims } = await auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+
+  const columns = [
+    {
+      header: "Tên sự kiện",
+      accessor: "title",
+    },
+    {
+      header: "Lớp",
+      accessor: "class",
+      // className: "hidden md:table-cell",
+    },
+    {
+      header: "Ngày",
+      accessor: "date",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Bắt đầu",
+      accessor: "startTime",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Kết thúc",
+      accessor: "endTime",
+      className: "hidden md:table-cell",
+    },
+    ...(role === "admin"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: EventList) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaSkyLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
+      <td className="">{item.class?.name || "-"}</td>
+      <td className="hidden md:table-cell">
+        {new Intl.DateTimeFormat("vi-VN").format(item.startTime)}
+      </td>
+      <td className="hidden md:table-cell">
+        {item.startTime.toLocaleString("vi-VN", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        })}
+      </td>
+      <td className="hidden md:table-cell">
+        {item.endTime.toLocaleString("vi-VN", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        })}
+      </td>
+      <td>
+        <div className="flex items-center gap-2">
+          {role === "admin" && (
+            <>
+              <FormContainer table="event" type="update" data={item} />
+              <FormContainer table="event" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
   const { page, ...queryParams } = (await searchParams) || {};
 
   const p = page ? parseInt(page) : 1;
