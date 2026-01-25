255840ada13b86de92e086130cd1089b5fdcdee8
phuongez <duyphuong.hvnh@gmail.com>
2026-01-25 08:57:08 +0700

    add teacher role in assignment

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -131,9 +131,10 @@ export default async function AssignmentListPage({
         </h1>
         <div className="flex items-center gap-4">
           <TableSearch />
-          {role === "admin" && (
-            <FormContainer table="assignment" type="create" />
-          )}
+          {role === "admin" ||
+            (role === "teacher" && (
+              <FormContainer table="assignment" type="create" />
+            ))}
         </div>
       </div>
 
